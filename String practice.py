13285fa005bb99d901323dcf47ffc05e97a505fd
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-02-22 22:17:26 +0500

    Add files via upload

---
+++ String practice.py
@@ -0,0 +1,13 @@
+#String Length
+x = "Urooj"
+print(len(x))
+#Check String(in used)
+Age ="I am 20 year old"
+print("hello" in Age)
+#task
+Name = "My name is Urooj Fatima"
+if "Urooj" in Name:
+    print("Yes 'Urooj' is present")
+#Check if NOT
+text = "The weather is cloudy, today"
+print("Good" not in text)
