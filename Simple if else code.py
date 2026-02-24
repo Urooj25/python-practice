e0e0a36e3c8bacc64bfb1f252a0ea4c4807549f6
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-02-24 18:41:19 +0500

    Add files via upload
    A simple Python program that demonstrates the use of if-else statements. The program takes a student's name, marks, and subject as input and provides a message based on the marks: congratulates for high marks, acknowledges passing marks, or encourages to try again. Perfect for beginners learning conditional statements in Python.

---
+++ Simple if else code.py
@@ -0,0 +1,10 @@
+Name = input("Enter your name: ")
+Marks = int(input("Enter your Marks: "))
+Course = input("Enter your Subject:")
+if Marks>80:
+     print("Congratulation, you scored highest marks")
+elif Marks>70  and Marks<80 :
+        print("Congratulation, you are pass")
+else: 
+  print("Sorry, try next time")
+
