61ceec7720337fbcd72cb394222114f55dd00cbe
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-22 23:04:44 +0500

    Add files via upload

---
+++ pythonstrings.py
@@ -0,0 +1,16 @@
+
+#Negative indexing
+a = "I love you"
+print(a[-5:-2])
+#Modify strings
+b = "my name is urooj"
+print(b.upper())
+#lowercase
+c = "MY NAME IS UROOJ"
+print(c.lower())
+#Remove whitespace from beging and end only
+d = "Hi, how are you!"
+print(d.strip())
+#Replace string
+e = "I am so happy for you"
+print(e.replace("happy", "Sad"))
