67fdc935044a3175e75f20c83b36e8ca3fc2c047
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-22 23:20:16 +0500

    Add files via upload

--- pythonstrings.py
+++ pythonstrings.py
@@ -1,16 +1,11 @@
+#Strings concatentation
+a = "Urooj"
+b = "Fatima"
+c = a +" "+b
+print(c)
+#To add a space between them, add a " "
+#Python - Format - Strings
+x = 35
+y = f"My age is:{x}"
 
-#Negative indexing
-a = "I love you"
-print(a[-5:-2])
-#Modify strings
-b = "my name is urooj"
-print(b.upper())
-#lowercase
-c = "MY NAME IS UROOJ"
-print(c.lower())
-#Remove whitespace from beging and end only
-d = "Hi, how are you!"
-print(d.strip())
-#Replace string
-e = "I am so happy for you"
-print(e.replace("happy", "Sad"))
+print(y)
