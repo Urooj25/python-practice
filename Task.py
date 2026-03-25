56097e4c519e67d849fdf5cd830004bdd1e74c80
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-25 14:00:52 +0500

    Add files via upload
    Lab task done

---
+++ Task.py
@@ -0,0 +1,77 @@
+#task 1
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print(x)
+#task 2 casefold()
+txt = "Hello, and  welcome to my world."
+x = txt.casefold()
+print(x)
+#center() task 3
+txt = "banana"
+x = txt.center(20)
+print(x)    
+#string.center task 4
+txt = "Banana"
+x = txt.center(8, "L")
+print(x)
+#task count() task 5
+txt = "I love apples, apples are my favourite fruits"
+x = txt.count("a")
+print(x)
+#task string.count(value,star,end) task 6
+txt = ("I love apple, apple is my fav fruit")
+x = txt.count("apple",1,15)
+print(x)
+# task 7 endswith()
+txt = "Hello, welcome to my world."
+x = txt.endswith(".")     
+print(x)     
+#task 8
+txt = "Hello, welcome to my world"   
+x = txt.endswith(("world", "castle"))  
+print(x)
+#task
+txt = "hELLO, welcome to my world"
+x = txt.find("welcome")
+print(x)
+#task
+txt = "Hello, welcome to my world" 
+x = txt.find("e" , 5, 10)
+print(x)
+#format()
+txt = "for only {price:.2f} dollers!"
+print(txt.format(price = 49))
+#named indexes
+txt1 = "My name is {fname}, I' am {age} ".format(fname = "John", age = 36)
+print(txt1)
+#numbered indexes
+txt2 = "My name is {0}, I' am  {1}".format("John", 36)
+print(txt2)
+#empty placeholders
+txt3 = "My name is {}, I' am {}".format("John",36)
+print(txt3)
+#Boolean type 
+x = True
+print(type(x))
+#compairon operators
+print(10>9)
+print(10==9)
+print(10<9)
+#Evaluate values and variables (importent in security domain)
+x = "Hello"
+y = 25
+print(bool(x))
+print(bool(y))#if variable is empty non or false it returns false
+#Operators
+x = 10
+y = 5
+#Floor division
+print(x/y)
+print(x//y)
+#And and or operator convert to binary then add according to & operator
+x = 3
+x &= 2
+print(x)
+x = 5
+x |= 2
+print(x)
