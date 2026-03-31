5f70b49c55412a9ad8201b463677142910326d5a
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-31 23:59:33 +0500

    Add files via upload

--- sets.py
+++ sets.py
@@ -3,3 +3,6 @@ print(type(people))
 #Duplicates remove
 number = {1,2,2,3,3,4,5,4,6}
 print(number)
+# The values True and 1 are considered the same value in sets, and are treated as duplicates:
+number = {1,2,3,True,5,7,9,4,3,10,9,8}
+print(number)
