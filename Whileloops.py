f4edd0743006890b023940b02e85dc8348c6c52e
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-03 19:05:02 +0500

    Add files via upload

---
+++ Whileloops.py
@@ -0,0 +1,34 @@
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+#task
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+#Task 
+i = 1
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+ #With the else statement we can run a block of code once when the condition no longer is true:
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer smaller")
+#Code challenge
+i = 0
+while i < 6:
+ i += 1
+ if i == 3:
+  continue
+  print(i)
+
+ 
