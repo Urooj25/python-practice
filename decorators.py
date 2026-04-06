1f7538347c7a53e613f40bda1c308f1102072097
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-06 20:45:16 +0500

    Add files via upload

---
+++ decorators.py
@@ -0,0 +1,42 @@
+#Decorators let you add extra behavior to a function, without changing the function's code.
+#Define the decorator first, then apply it with @decorator_name above the function.
+def changecase(func):
+    def myinner():
+        return func().upper()
+    return myinner
+@changecase
+def myfunction():
+    return "hello urooj"
+print(myfunction())
+#multiple
+def changecase(func):
+    def inner():
+        return func().upper()
+    return inner
+@changecase
+def myfunction():
+    return "hello Urooj"
+@changecase
+def greeting():
+    return "Hello"
+print(myfunction())
+print(greeting())
+#Arguments in the Decorated Function
+def changecase(func):
+    def inner(x):
+        return func(x).upper()
+    return inner
+@changecase
+def myfunction(name):
+    return "Hello  " + name
+print(myfunction("John"))
+#*args and **kwargs
+def myfunction(func):
+    def inner(*arg,**karg):
+        return func(*arg,**karg).upper()
+    return inner
+@myfunction
+def function(name):
+    return "hello " + name
+print(function("john"))
+
