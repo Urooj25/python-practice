7379baa74c98fc9446631f1801ad790172b1adbe
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-21 23:54:24 +0500

    Add files via upload

--- generators2.py
+++ generators2.py
@@ -8,6 +8,34 @@ print(list(gen_exp))
 gen_exp = (x*x for x in range(5,10))
 for x in gen_exp:
         print(x)
-
-
+#Using a generator expression with sum:
+total = sum(x*x for x in range(5,10))
+print(total)
+#Fibonacci Sequence Generator
+#0, 1, 1, 2, 3, 5, 8, 13, 21, 34... next number previous two numbers kay sum say bnta ha
+#"Without Running Out of Memory" Ka Matlab:
+#fib_list = [0,1,1,2,3,5,8,13.....] # infinite list = memory full!
+#Fibonacci generator kabhi khatam nahi hota – while True ki wajah se, aur memory bhi nahi bharta kyunki ek ek value deta hai! 
+def Fibonacci():
+    a,b =0,1
+    while True:
+        yield a
+        a,b = b,a+b
+gen = Fibonacci()
+for _ in range(100):
+    print(next(gen))
+    ''' ❌ Yeh dangerous hai
+while True:
+    print("hello")   # rokne ka koi tarika nahi! crash!'''
+#Generator Methods
+#The send() method allows you to send a value to the generator:
+def echo_generator():
+    while True:
+        recieved = yield
+        print(("recieved",recieved))
+gen = echo_generator()
+next(gen)#Prime the generator
+gen.send("Hello")
+gen.send("Urooj")
+#close() Method
 
