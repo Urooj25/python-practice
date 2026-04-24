408270503388a3f06a4520af1c952542829d73bc
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-24 17:29:51 +0500

    Add files via upload

---
+++ math.py
@@ -0,0 +1,24 @@
+#Built-in Math Functions
+#The min() and max() functions can be used to find the lowest or highest value in an iterable:
+x = min(5,10,20)
+y = max(50,70,100)
+print(x,y)
+#The abs() function returns the absolute (positive) value of the specified number:
+x = abs(-7.25)
+print(x)
+x = pow(3,9)
+print(x)
+#The Math Module
+import math
+x = math.sqrt(121)
+print(x)
+#The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+import math
+x = math.ceil(5.75)#round to upward
+y = math.floor(5.75)#round to lower
+print(x)
+print(y)
+#The math.pi constant, returns the value of PI (3.14...):
+import math
+x = math.pi
+print(x)
