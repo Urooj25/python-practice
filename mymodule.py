444be4df3481f3e1310427b5890c444be4ae2975
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-24 16:45:49 +0500

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    print("hello", name)
+#module bna kr ek save may file kr lu r phir beshk jitni dfa used kro bar bar nhi likhna pra gha
+#The module can contain functions, as already described, but also variables of all types (arrays, dictionaries, objects etc):
+person1= {
+    "name" : "Urooj",
+    "class" : "c",
+    "rollno" : 575137
+}
+#
