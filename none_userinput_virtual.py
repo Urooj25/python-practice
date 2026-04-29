8a9874f88956304032b7e6b2f559bdde74c2d123
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-04-29 13:08:52 +0500

    Add files via upload

--- none_userinput_virtual.py
+++ none_userinput_virtual.py
@@ -18,5 +18,30 @@ def myfunction(x):
     return x
 x = myfunction(x)
 print(x)
+#User input
+print("Enter your name:")
+name = input()
+print(f"my name is: {name}")
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+#To find the square root, the input has to be converted into a number:
+import math
+x = input("Enter a number:")
+y = math.sqrt(float(x))
+print(f"the square root of {x} is {y}")
+#validate input
+y = True
+while y == 0:#Loop kbhi chal hi nhi
+    x = input("Enter a number:")
+    try:
+        x = float(x);
+        y = False
+    except:
+        print("Wrong input, try again")
+print("Thank you")
 
-    
+
