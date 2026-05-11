6b7d203df4506f91a3fa11398f730aa944fb6d62
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-05-12 00:10:19 +0500

    Add files via upload

--- Exception_handling.py
+++ Exception_handling.py
@@ -4,8 +4,30 @@
 #Common errors in Python:
 '''Dividing by zero,Open afile that doesnot exist,wrong input example needs an name and we enter int'''
 '''Jab aapke code mein error aaye aur aap ne khud koi plan nahi banaya us error ke liye — to Python ka apna system woh error pakad kar screen par message dikhata hai aur program band kar deta hai.'''
-def divide(a,b):#calculator example
-    result = a/b
+def divide(a, b):
+    result = a / b
     return result
+
+x = divide(5, 0)
+print(x)
+#Check for valid input first
+def divide(a,b):
+    if b == 0:
+        result = 'Error: cannot divide by zero'
+        return result
+    else:
+        result = a/b
+        return result
 x = divide(5,0)
-print(x)#ZeroDivisionError: division by zero
+print(x)
+#hat if “b” is not a number?
+def divide(a,b):
+    if (type(b) is not int and type(b) is not float):
+        result = 'Error:divisor is not a number'
+        return result
+    elif b == 0:
+        result = "Error: Cannot divide by zero"
+        return result
+x = divide(5,0)
+print(x)
+
